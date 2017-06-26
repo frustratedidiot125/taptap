18450dde6e75ceb0173449b5a15ebf30406c8763
frustratedidiot123 <frustratedidiot123@users.noreply.github.com>
2017-06-26 17:27:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -89,8 +89,8 @@ alexaApp.intent("GetNewFactIntent", {
                   "utterances": [
       "a fact", "tell me a fact", "tell me a fun fact", "give me a fact", "give me a fake fact", "tell me trivia", "tell me some fake trivia", "give me trivia", "give me fake trivia", "tell me something", "give me something", "tell me a fake fun fact", 
                 "tell me something interesting", "tell me something new", "give me a fun fact", "enlighten me", "enlighten us", "tell me something that isn't true", "tell me something", "give me another", "give me another fact", "can i have another"
-    ]},
-                var factArr = data;
+    ]};
+var factArr = data;
 var factIndex = Math.floor(Math.random() * factArr.length);
 var randomFact = factArr[factIndex];
   response.say(GET_FACT_MESSAGE + randomFact + HELP_MESSAGE).reprompt(HELP_REPROMPT).shouldEndSession(false));
