ed6500fd8a210bef7adaba73aa93164242afa03e
frustratedidiot123 <frustratedidiot123@users.noreply.github.com>
2017-07-26 14:43:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // ALWAYS setup the alexa app and attach it to express before anything else.
-var alexaApp = new alexa.app("test");
+var alexaApp = new alexa.app("moo");
 
 alexaApp.express({
   expressApp: app,
@@ -29,36 +29,28 @@ app.set("view engine", "ejs");
 //=========================================================================================================================================
 //TODO: The items below this comment need your attention.
 //=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
-//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
-var APP_ID = undefined;
-
-var SKILL_NAME = "Fake fun facts";
-var GET_FACT_MESSAGE = "Little known fact.  ";
-var HELP_MESSAGE = "  You can say give me another fact, or, say stop to exit. which would you like?";
-var HELP_REPROMPT = "You can say, give me another fact, or stop to exit.";
+var spinmessage = "Spinning. 
+var HELP_MESSAGE = "  You can say spin again, or, stop to exit.?";
+var HELP_REPROMPT = "You can say, spin, spin again, or stop to exit. Which would you like?";
 var STOP_MESSAGE = "Goodbye!";
 
 //=========================================================================================================================================
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
 //=========================================================================================================================================
 var data = [
-    "Jeremiah was a turtle, not a bullfrog.", // He was a good friend to many, but not me.",
-    "There are 97 known varieties of grapefruit. They all taste the same.",
-    "The second happiest animal on earth is the zebra. The first is the lion.",
-    "Widget was considered an offensive word until 1858. Doodad was similarly outlawed by several local US statutes until 1911.",
-    "There is no spoon. There are only sporks.",
-    "3 out of every 5 superstitions tested are backed as true by scientific evidence. In the case of the remaining 2, the scientists in question stepped on cracks, suffered broken vertebrae, and were unable to complete their research. ",
-    "93 percent of all statistics are made up on the spot. The other 7 percent are not statistically significant.",
-    "Goldfish can live up to 300 years if cared for properly. Their memories, however, only last about 7 seconds.",
-    "The Sun is an almost perfect rectangle. Diffraction in Earth's atmosphere makes it look spherical.",
-    "The U.S. military once spent 2.2 billion dollars designing a submarine for giraffes.",
-    "Giraffes are excellent underwater swimmers.",
-    "The temperature at the very center of the Sun can precipitously drop to reach negative 15 degrees Celsius.",
-    "There are tribes in ancient russia that regularly rode wild boars into battle. ",
-    "Attack rabbits are real. Beware."
-];
+    "A cow says moo. Then it makes chomping noises.", // He was a good friend to many, but not me.",
+    "A rooster goes, ah say ah say ah say ah say.",
+    "A rabbit goes, what's up doc. The rest of the rabbits stare silently.",
+    "A giraffe is usually too tall to hear.",
+    "A cat goes growl. It just sounds like meow.",
+  //meow. That's usually an indication they want to eat their owner. ",
+    "A gerbil goes squeak. Then it bites your hand.",
+    "A dog goes grrr.",
+    "A goldfish goes in circles.",
+    "A capybara goes splash.",
+    "A PC goes ding!",
+    "A horse goes and goes and goes"
+    ];
 
 var randomFact = function () {
   var factArr = data;
@@ -89,27 +81,20 @@ alexaApp.launch(function(request, response) {
 //var factArrA = data;
 // var factIndexA = Math.floor(Math.random() * factArrA.length);
 // var randomFactA = factArrA[factIndexA];
-    response.say(GET_FACT_MESSAGE + randomFact()+HELP_MESSAGE).reprompt(HELP_REPROMPT).shouldEndSession(false);
+    response.say(randomFact()+HELP_MESSAGE).reprompt(HELP_REPROMPT).shouldEndSession(false);
 });
 
-alexaApp.intent("GetNewFactIntent", {
+alexaApp.intent("Spin", {
   "slots": {},
                   "utterances": [
-      "a fact", "tell me a fact", "tell me a fun fact", "give me a fact", "give me a fake fact", "tell me trivia", "tell me some fake trivia", "give me trivia", "give me fake trivia", "tell me something", "give me something", "tell me a fake fun fact", 
-                "tell me something interesting", "tell me something new", "give me a fun fact", "enlighten me", "enlighten us", "tell me something that isn't true", "tell me something", "give me another", "give me another fact", "can i have another"
+      "spin", '"spin again" 
     ]
   },
                 function (request, response){
-  response.say(GET_FACT_MESSAGE + randomFact() + HELP_MESSAGE).reprompt(HELP_REPROMPT).shouldEndSession(false)});
+  response.say(spinmessage + randomFact() + HELP_MESSAGE).reprompt(HELP_REPROMPT).shouldEndSession(false)});
 
     
-   // 'GetNewFactIntent': function () {
-     //   var factArr = data;
-      //  var factIndex = Math.floor(Math.random() * factArr.length);
-     //   var randomFact = factArr[factIndex];
-    //    var speechOutput = GET_FACT_MESSAGE + randomFact;
-     //   this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
-//    },
+   
 
 alexaApp.intent("AMAZON.HelpIntent", {
   "slots": {} },
