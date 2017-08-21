41fc62eb82450addd26ac52e46b361d039590adf
frustratedidiot125 <30579982+frustratedidiot125@users.noreply.github.com>
2017-08-21 08:59:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ alexaApp.launch(function(req, res) {
 
   
   res.session('step', 0);
-  var prompt = "Hi there! I can tell you how to build an eclipse projection box. Just say next to begin, or say the number of the step you wish to resume. Say stop at any time to exit. ";
+  var prompt = "Hi there! I can tell you how to build an eclipse projection box. Just say begin, or say the number of the step you wish to resume. Say next to go to the next step, and Say stop at any time to exit. ";
   res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
